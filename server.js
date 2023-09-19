cd8b99e392eb8872561eb77e0d66998721bdf140
Abhishek Bondre <abhibondre27@gmail.com>
2023-09-19 20:29:40 +0530

    changes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -22,7 +23,23 @@ app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
+// --------Deployment----------------------
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is Running Successfully");
+  });
+}
+// --------Deployment----------------------
+
 app.use(notFound);
+
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 4000;
